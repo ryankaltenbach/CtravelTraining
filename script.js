44c4bcb035a28681d3ab1947c3f30b79e071aede
ryankaltenbach <RyanKT@protonmail.com>
2019-09-28 13:32:46 -0700

    tree

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ function getRandomPosition() {
     var randomY = Math.floor(Math.random()*y);
     return [randomX,randomY];
 }
+
+// Tree script for drop down
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
